fea8c692e620887e38aa550338d1b9f4e913a12c
VrajShah2005 <vrajshah959@gmail.com>
2026-03-03 22:22:49 +0530

    Delete cartRoutes.js

--- cartRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const cartController = require('../controllers/cartController');
-
-// Route to add items to cart
-router.post('/add', cartController.addToCart);
-
-module.exports = router;
