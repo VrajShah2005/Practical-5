3d7c04ac5ea79618ceec1a16fce3efbeab631871
VrajShah2005 <vrajshah959@gmail.com>
2026-03-03 22:20:33 +0530

    Delete orderController.js

--- orderController.js
+++
@@ -1,8 +0,0 @@
-exports.placeOrder = (req, res, next) => {
-    try {
-        // Simple logic: assume order is successful
-        res.status(201).json({ success: true, message: "Order placed successfully!" });
-    } catch (err) {
-        next(err);
-    }
-};
