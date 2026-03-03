e6fcdae3285538103ac0f8c38fb071f9c0d146d8
VrajShah2005 <vrajshah959@gmail.com>
2026-03-03 22:21:18 +0530

    Delete orderRoutes.js

--- orderRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const orderController = require('../controllers/orderController');
-
-// Route to place an order
-router.post('/checkout', orderController.placeOrder);
-
-module.exports = router;
